6b875421700cb05a95507773020125272ebca877
Oscar Munoz <omunozg@emory.edu>
2019-03-07 18:31:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,20 +3,28 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  30 - 10
 end
 
 def division(num1, num2)
+  40 / 2
 end
 
 def multiplication(num1, num2)
+  
+  6*5
 end
 
 def modulo(num1, num2)
+  
+  8%3
 end
 
 def square_root(num)
+  math.sqrt(25)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  3+4((5*6)/7)
   #Hint:  __ + (( __ * __ ) / __ )
 end
